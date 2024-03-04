e32e878cc5304f8dc071a0acc04a7f3903be7cfa
Ross Dobson <56548895+ross-dobson@users.noreply.github.com>
2024-03-04 19:29:42 +0000

    test: add test planet letter

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,12 +2,21 @@ from ravest.model import Planet, Star
 import numpy as np
 import pytest
 
-known_good_params = {"p": 13.2,
-                     "k": 27,
-                     "e": 0.2,
-                     "w": np.pi/2,
-                     "tp": 4}
+@pytest.fixture
+def known_good_params():
+    return {"p": 13.2,
+            "k": 27,
+            "e": 0.2,
+            "w": np.pi/2,
+            "tp": 4}
 
 def test_planet_letter_valueerror_if_multiple_letters():
     with pytest.raises(ValueError):
         Planet(letter="abcdefg", basis="p k e w tp", params=known_good_params)
+def test_planet_letter_valuerror_if_not_isalpha():
+    with pytest.raises(ValueError):
+        Planet(letter="!", basis="p k e w tp", params=known_good_params)
+    with pytest.raises(ValueError):
+        Planet(letter="5", basis="p k e w tp", params=known_good_params)
+def test_planet_letter_good():
+    assert Planet(letter="b", basis="p k e w tp", params=known_good_params).letter == "b"
