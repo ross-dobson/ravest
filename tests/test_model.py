d9c17da0edf71451620a92504ff9cca63d5ad749
Ross Dobson <56548895+ross-dobson@users.noreply.github.com>
2024-03-05 11:21:53 +0000

    test: add test star rv

--- tests/test_model.py
+++ tests/test_model.py
@@ -23,9 +23,10 @@ def data_tarr():
     return np.arange(0,100,0.1)
 
 
-@pytest.fixture
-def good_planet():
+def good_planet1():
     return Planet(letter="b", basis="p k e w tp", params=known_params1())
+def good_planet2():
+    return Planet(letter="c", basis="p k e w tp", params=known_params2())
 
 def good_star():
     return Star(name="goodstar", mass=1)
@@ -43,19 +44,26 @@ def test_planet_letter_good():
 
 def test_star_num_planets():
     star = good_star()
-    star.add_planet(Planet(letter="b", basis="p k e w tp", params=known_params1()))
+    star.add_planet(good_planet1())
     new_planet = Planet(letter="b", basis="p k e w tp", params={"p":10, "k":10, "e":0, "w":0, "tp":0})
     star.add_planet(new_planet)
     assert star.num_planets == 1  # check the new planet "b" has replaced the old "b"
 
 def test_rv_pkewtp_eccentric():
-    b = Planet(letter="b", basis="p k e w tp", params=known_params1())
+    b = good_planet1()
     rv1 = b.radial_velocity(data_tarr())
     assert list(data_rv1()) == list(rv1)
 
 def test_rv_pkewtp_circular():
-    c = Planet(letter="c", basis="p k e w tp", params=known_params2())
+    c = good_planet2()
     rv2 = c.radial_velocity(data_tarr())
     assert list(data_rv2()) == list(rv2)
 
-
+def test_star_rv():
+    b = good_planet1()
+    c = good_planet2()
+    star = good_star()
+    star.add_planet(b)
+    star.add_planet(c)
+    star_rv = star.radial_velocity(data_tarr())
+    assert list(data_rv1()+data_rv2()) == list(star_rv)
