65c26af85b9dd2ccf9e472ef9bdb43e320ba6720
Ross Dobson <56548895+ross-dobson@users.noreply.github.com>
2024-07-01 20:49:26 +0100

    test(test_model): update tests to work with new parameterisation

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,15 +1,16 @@
-from ravest.model import Planet, Star
+from ravest.model import Planet, Star, Trend
+from ravest.param import Parameterisation
 import numpy as np
 import pytest
 
 def known_params1():
-    return {"p": 13.2,
+    return {"per": 13.2,
             "k": 27,
             "e": 0.2,
             "w": 0.9*np.pi,
             "tp": 2}
 def known_params2():
-    return {"p": 1.5,
+    return {"per": 1.5,
             "k": 10,
             "e": 0,
             "w": np.pi/2,
@@ -24,28 +25,31 @@ def data_tarr():
 
 
 def good_planet1():
-    return Planet(letter="b", basis="p k e w tp", params=known_params1())
+    return Planet(letter="b", parameterisation=Parameterisation("per k e w tp"), params=known_params1())
 def good_planet2():
-    return Planet(letter="c", basis="p k e w tp", params=known_params2())
+    return Planet(letter="c", parameterisation=Parameterisation("per k e w tp"), params=known_params2())
+
+def good_trend():
+    return Trend({"g":0, "gd":0, "gdd":0})
 
 def good_star():
     return Star(name="goodstar", mass=1)
 
 def test_planet_letter_valueerror_if_multiple_letters():
     with pytest.raises(ValueError):
-        Planet(letter="abcdefg", basis="p k e w tp", params=known_params1())
+        Planet(letter="abcdefg", parameterisation=Parameterisation("per k e w tp"), params=known_params1())
 def test_planet_letter_valuerror_if_not_isalpha():
     with pytest.raises(ValueError):
-        Planet(letter="!", basis="p k e w tp", params=known_params1())
+        Planet(letter="!", parameterisation=Parameterisation("per k e w tp"), params=known_params1())
     with pytest.raises(ValueError):
-        Planet(letter="5", basis="p k e w tp", params=known_params1())
+        Planet(letter="5", parameterisation=Parameterisation("per k e w tp"), params=known_params1())
 def test_planet_letter_good():
-    assert Planet(letter="b", basis="p k e w tp", params=known_params1()).letter == "b"
+    assert Planet(letter="b", parameterisation=Parameterisation("per k e w tp"), params=known_params1()).letter == "b"
 
 def test_star_num_planets():
     star = good_star()
     star.add_planet(good_planet1())
-    new_planet = Planet(letter="b", basis="p k e w tp", params={"p":10, "k":10, "e":0, "w":0, "tp":0})
+    new_planet = Planet(letter="b", parameterisation=Parameterisation("per k e w tp"), params=known_params2())
     star.add_planet(new_planet)
     assert star.num_planets == 1  # check the new planet "b" has replaced the old "b"
 
@@ -65,7 +69,6 @@ def test_star_rv():
     star = good_star()
     star.add_planet(b)
     star.add_planet(c)
+    star.add_trend(good_trend())
     star_rv = star.radial_velocity(data_tarr())
     assert list(data_rv1()+data_rv2()) == pytest.approx(list(star_rv))
-
-# TODO tests for conversion tp <-> tc
